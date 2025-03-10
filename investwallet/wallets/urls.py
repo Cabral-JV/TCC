6b71f023e4e6b280f4731b3a31d062dccffe2bd5
João Victor <jvccabral96@gmail.com>
2025-03-10 17:23:49 -0300

    - Criando as paginas de registro e de login. - Criando a model

--- investwallet/wallets/urls.py
+++ investwallet/wallets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views
+from .views import home, login_view, register_view, logout_view
 
 app_name = 'wallets'
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", home, name="home"),
+    path("login/", login_view, name="login"),
+    path("register/", register_view, name="register"),
+    path("logout/", logout_view, name="logout"),
 ]
--- investwallet/wallets/views.py
+++ investwallet/wallets/views.py
@@ -1,4 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib import messages
 
 def home(request):
     return render(request, 'wallets/home.html')
+
+# View para Login
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("home")  # Se já estiver logado, redireciona para a home
+
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "Login realizado com sucesso!")  # Mensagem de sucesso
+            return redirect("home")  
+        else:
+            messages.error(request, "Usuário ou senha inválidos.")  # Mensagem de erro
+
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, "wallets/login.html", {"form": form})
+
+# View para Registro
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect("home")  # Se já estiver logado, redireciona para a home
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Cadastro realizado com sucesso! Bem-vindo(a) ao InvestWallet.")
+            return redirect("home")
+        else:
+            messages.error(request, "Erro no cadastro. Verifique os dados informados.")  # Mensagem de erro
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, "wallets/register.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "Você saiu da conta.")
+    return redirect("home")
